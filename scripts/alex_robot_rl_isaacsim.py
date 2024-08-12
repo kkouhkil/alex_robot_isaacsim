c016d5370ee0ad7b14dc0eafc15eed05716cf39d
Keyhan Kouhkiloui <kkouhkil@ed.ac.uk>
2024-08-12 15:37:43 +0100

    alex robot simulation - isaacsim - in progress

--- scripts/alex_robot_rl_isaacsim.py
+++ scripts/alex_robot_rl_isaacsim.py
@@ -1,26 +1,9 @@
-# Copyright (c) 2022-2024, The Isaac Lab Project Developers.
-# All rights reserved.
-#
-# SPDX-License-Identifier: BSD-3-Clause
-
-"""
-This script demonstrates different single-arm manipulators.
-
-.. code-block:: bash
-
-    # Usage
-    ./isaaclab.sh -p source/standalone/demos/arms.py
-
-"""
-
-"""Launch Isaac Sim Simulator first."""
-
 import argparse
 
 from omni.isaac.lab.app import AppLauncher
 
 # add argparse arguments
-parser = argparse.ArgumentParser(description="This script demonstrates different single-arm manipulators.")
+parser = argparse.ArgumentParser(description="Alex Robot")
 # append AppLauncher cli args
 AppLauncher.add_app_launcher_args(parser)
 # parse the arguments
@@ -180,42 +163,31 @@ ALEX_ROBOT_CFG = ArticulationCfg(
     soft_joint_pos_limit_factor=1.0,
 )
 
-##
-# Pre-defined configs
-##
-# isort: off
-from omni.isaac.lab_assets import (
-    FRANKA_PANDA_CFG,
-    UR10_CFG,
-    KINOVA_JACO2_N7S300_CFG,
-    KINOVA_JACO2_N6S300_CFG,
-    KINOVA_GEN3_N7_CFG,
-    SAWYER_CFG,
-)
-
-# isort: on
-
-
 def define_origins(num_origins: int, spacing: float) -> list[list[float]]:
     """Defines the origins of the the scene."""
     # create tensor based on number of environments
     env_origins = torch.zeros(num_origins, 3)
+
     # create a grid of origins
     num_rows = np.floor(np.sqrt(num_origins))
     num_cols = np.ceil(num_origins / num_rows)
+
     xx, yy = torch.meshgrid(torch.arange(num_rows), torch.arange(num_cols), indexing="xy")
+
     env_origins[:, 0] = spacing * xx.flatten()[:num_origins] - spacing * (num_rows - 1) / 2
     env_origins[:, 1] = spacing * yy.flatten()[:num_origins] - spacing * (num_cols - 1) / 2
     env_origins[:, 2] = 0.0
+
     # return the origins
     return env_origins.tolist()
 
-
 def design_scene() -> tuple[dict, list[list[float]]]:
+
     """Designs the scene."""
     # Ground-plane
     cfg = sim_utils.GroundPlaneCfg()
     cfg.func("/World/defaultGroundPlane", cfg)
+
     # Lights
     cfg = sim_utils.DomeLightCfg(intensity=2000.0, color=(0.75, 0.75, 0.75))
     cfg.func("/World/Light", cfg)
@@ -224,13 +196,15 @@ def design_scene() -> tuple[dict, list[list[float]]]:
     # Each group will have a mount and a robot on top of it
     origins = define_origins(num_origins=1, spacing=2.0)
 
-    # Origin 1 with Alex
+    # Origin with Alex
     prim_utils.create_prim("/World/Origin", "Xform", translation=origins[0])
-    # -- Table
+
+    # -- Stand
     cfg = sim_utils.UsdFileCfg(
         usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/Mounts/Stand/stand_instanceable.usd", scale=(2.0, 2.0, 2.0)
     )
-    cfg.func("/World/Origin/Table", cfg, translation=(0.0, 0.0, 1.03))
+    cfg.func("/World/Origin/Stand", cfg, translation=(0.0, 0.0, 1.03))
+
     # -- Robot
     alex_robot_cfg = ALEX_ROBOT_CFG.replace(prim_path="/World/Origin/Robot")
     alex_robot_cfg.init_state.pos = (0.0, 0.0, 1.03)
@@ -242,73 +216,92 @@ def design_scene() -> tuple[dict, list[list[float]]]:
     }
     return scene_entities, origins
 
-
 def run_simulator(sim: sim_utils.SimulationContext, entities: dict[str, Articulation], origins: torch.Tensor):
+
     """Runs the simulation loop."""
     # Define simulation stepping
     sim_dt = sim.get_physics_dt()
     sim_time = 0.0
     count = 0
+
     # Simulate physics
     while simulation_app.is_running():
         # reset
         if count % 200 == 0:
+
             # reset counters
             sim_time = 0.0
             count = 0
+
             # reset the scene entities
             for index, robot in enumerate(entities.values()):
+
                 # root state
                 root_state = robot.data.default_root_state.clone()
                 root_state[:, :3] += origins[index]
                 robot.write_root_state_to_sim(root_state)
+
                 # set joint positions
                 joint_pos, joint_vel = robot.data.default_joint_pos.clone(), robot.data.default_joint_vel.clone()
                 robot.write_joint_state_to_sim(joint_pos, joint_vel)
+
                 # clear internal buffers
                 robot.reset()
+
             print("[INFO]: Resetting robots state...")
         # apply random actions to the robots
         for robot in entities.values():
+
             # generate random joint positions
             joint_pos_target = robot.data.default_joint_pos + torch.randn_like(robot.data.joint_pos) * 0.1
             joint_pos_target = joint_pos_target.clamp_(
                 robot.data.soft_joint_pos_limits[..., 0], robot.data.soft_joint_pos_limits[..., 1]
             )
+
             # apply action to the robot
             robot.set_joint_position_target(joint_pos_target)
+
             # write data to sim
             robot.write_data_to_sim()
+
         # perform step
         sim.step()
+
         # update sim-time
         sim_time += sim_dt
         count += 1
+
         # update buffers
         for robot in entities.values():
             robot.update(sim_dt)
 
 
 def main():
-    """Main function."""
+
     # Initialize the simulation context
     sim_cfg = sim_utils.SimulationCfg()
     sim = sim_utils.SimulationContext(sim_cfg)
+
     # Set main camera
     sim.set_camera_view([3.5, 0.0, 2.5], [-180 * math.pi/180, 0 * math.pi/180, 0 * math.pi/180])
+
     # design scene
     scene_entities, scene_origins = design_scene()
     scene_origins = torch.tensor(scene_origins, device=sim.device)
+
     # Play the simulator
     sim.reset()
+
     # Now we are ready!
     print("[INFO]: Setup complete...")
+
     # Run the simulator
     run_simulator(sim, scene_entities, scene_origins)
 
-
 if __name__ == "__main__":
+
     # run the main function
     main()
+    
     # close sim app
     simulation_app.close()
